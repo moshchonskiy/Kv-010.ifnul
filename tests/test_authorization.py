5f93465959f35303cdb57f3adbe86e8e33c0194b
moshchonskiy <moshchonskiy@gmail.com>
2015-10-05 16:17:46 +0300

    new selectors requirements.txt

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,30 +1,30 @@
 __author__ = 'Evgen'
 import pytest
+import allure
 from model.user import User
 from selenium.webdriver.common.keys import Keys
 from conftest import app
 
-
 def test_valid_login(app):
-    app.ensure_logout()
-    app.login(User.Admin())
-    assert app.is_logged_in()
-
+    with pytest.allure.step('Valid login test'):
+        app.ensure_logout()
+        app.login(User.Admin())
+        assert app.is_logged_in()
 
 def test_remember_me_checkbox(app):
-    app.ensure_logout()
-    app.login(User.Admin(), True)
-    ip = app.internal_page
-    main_window_handle = ip.driver.current_window_handle
-    body = ip.driver.find_element_by_tag_name("body")
-    body.send_keys(Keys.CONTROL + 'n')
-    ip.driver.switch_to.window(main_window_handle)
-    all_windows = ip.driver.window_handles
-    ip.driver.close()
-    ip.driver.switch_to.window(all_windows[1])
-    ip.driver.get("http://localhost:9000/#/person/list")
-    assert app.is_logged_in()
-
+    with pytest.allure.step("Remember me checkbox test"):
+        app.ensure_logout()
+        app.login(User.Admin(), True)
+        ip = app.internal_page
+        main_window_handle = ip.driver.current_window_handle
+        body = ip.driver.find_element_by_tag_name("body")
+        body.send_keys(Keys.CONTROL + 'n')
+        ip.driver.switch_to.window(main_window_handle)
+        all_windows = ip.driver.window_handles
+        ip.driver.close()
+        ip.driver.switch_to.window(all_windows[1])
+        ip.driver.get("http://localhost:9000/#/person/list")
+        assert app.is_logged_in()
 
 def test_invalid_login(app):
     app.ensure_logout()
