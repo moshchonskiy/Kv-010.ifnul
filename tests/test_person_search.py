a5a851e4a1669d7d86a5abf17262c65ae7f2b9e4
romanchenkovika <romanchenko366@gmail.com>
2015-10-07 19:27:24 +0300

    no time.sleep()'s

--- tests/test_person_search.py
+++ tests/test_person_search.py
@@ -1,39 +1,29 @@
 # coding: utf8
-import time
 from model.user import User
-from pages.persons.persons_page import PersonsPage
-__author__ = 'vika'
+from pages.persons.persons_page import GIVEN_SURNAME, GIVEN_PERSON_ID, GIVEN_NUM_OS, GIVEN_SERIES_OS
 
 
-GIVEN_SURNAME = u'Іваненко'
-GIVEN_PERSON_ID = '14'
-GIVEN_NUM_OS = '34654654'
-GIVEN_SERIES_OS = '32'
-
-SLEEP_TIME = 7
-
-def test_is_login(app):
+def login(app):
     app.ensure_logout()
     app.login(User.Admin(), True)
-    assert app.internal_page.is_this_page
+
 
 def test_surname_search(app):
+    login(app)
     person_page = app.persons_page
     person_page.try_get_choose_surname().click()
     person_page.try_get_input_group().clear()
     person_page.try_get_input_group().send_keys(GIVEN_SURNAME)
     person_page.try_get_ok_button().click()
-    time.sleep(SLEEP_TIME)
-    assert GIVEN_SURNAME in person_page.try_get_expected_surname().text
+    #the 1st word (surname) will be given
+    assert person_page.try_get_expected_surname().text.partition(' ')[0] == GIVEN_SURNAME
 
 def test_person_id_search(app):
     person_page = app.persons_page
-    #persone_page = PersonsPage()
     person_page.try_get_choose_person_id().click()
     person_page.try_get_input_group().clear()
     person_page.try_get_input_group().send_keys(GIVEN_PERSON_ID)
     person_page.try_get_ok_button().click()
-    time.sleep(SLEEP_TIME)
     assert person_page.try_get_expected_person_id().text == GIVEN_PERSON_ID
 
 def test_num_os_search(app):
@@ -42,7 +32,6 @@ def test_num_os_search(app):
     person_page.try_get_input_group().clear()
     person_page.try_get_input_group().send_keys(GIVEN_NUM_OS)
     person_page.try_get_ok_button().click()
-    time.sleep(SLEEP_TIME)
     assert person_page.try_get_expected_num_os().text == GIVEN_NUM_OS
 
 def test_series_os_search(app):
@@ -51,5 +40,4 @@ def test_series_os_search(app):
     person_page.try_get_input_group().clear()
     person_page.try_get_input_group().send_keys(GIVEN_SERIES_OS)
     person_page.try_get_ok_button().click()
-    time.sleep(SLEEP_TIME)
     assert person_page.try_get_expected_series_os().text == GIVEN_SERIES_OS
