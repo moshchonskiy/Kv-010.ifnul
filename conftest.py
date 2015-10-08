9b555d440b73527688ac142f7cfbd2e45d15eb76
ITsvetkoFF <tsvetkoff@i.ua>
2015-10-08 19:15:08 +0300

    conftest module

--- conftest.py
+++ conftest.py
@@ -11,17 +11,17 @@ def pytest_addoption(parser):
     parser.addoption("--base_url", action="store", default="http://194.44.198.221")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def browser_type(request):
     return request.config.getoption("--browser")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def base_url(request):
     return request.config.getoption("--base_url")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def app(request, browser_type, base_url):
     """
     Fixture is used to perform all tests, use it in your tests like >>>  def test_method(app)
