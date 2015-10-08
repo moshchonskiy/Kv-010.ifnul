44312532fdb5ae6ecf29648bde839353fb519933
acidroed <acidroed@ukr.net>
2015-10-08 22:18:45 +0300

    -add person test

--- pages/persons/add/person_papers_page.py
+++ pages/persons/add/person_papers_page.py
@@ -1,1 +1,102 @@
-__author__ = 'Evgen'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Deorditsa'
+
+from person_base_page import AddPersonPage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+
+class AddPersonPapersPage(AddPersonPage):
+
+    ADD_DOCUMENT_BUTTON = (By.XPATH, "//div[@class='row']//div[@class='col-xs-2']//button")
+    SAVE_DOCUMENT_BUTTON = (By.XPATH, "//button[@ng-click='addToTable()']")
+    DOCUMENT_CATEGORY_SELECT = (By.XPATH, "//select[@ng-model='currentObj.abbrName']")
+    DOCUMENT_NAME_SELECT = (By.XPATH, "//select[@ng-model='currentObj.paperTypeId']")
+    ALL_DOCUMENT_NAMES_SELECT = (By.XPATH, "//select[@ng-model='currentObj.paperTypeId']//option")
+    DOCUMENT_SERIES_INPUT = (By.XPATH, "//input[@ng-model='currentObj.docSeries']")
+    DOCUMENT_NUMBER_INPUT = (By.XPATH, "//input[@ng-model='currentObj.docNum']")
+    DOCUMENT_DAY_OF_ISSUE_CHOSER = (By.XPATH, "//input[@ng-model='currentObj.docDate']")
+    DOCUMENT_ISSUED_BY = (By.XPATH, "//input[@ng-model='currentObj.docIssued']")
+    DOCUMENT_IS_ORIGINAL = (By.XPATH, "//input[@ng-model='currentObj.isChecked']")
+    DOCUMENT_IS_FOREIGN = (By.XPATH, "//input[@ng-model='currentObj.isForeign']")
+
+    @property
+    def is_this_page(self):
+        return self.is_element_visible(self.DOCUMENT_NAME_SELECT)
+
+    @property
+    def press_add_new_document_button(self):
+        self.driver.find_element(*self.ADD_DOCUMENT_BUTTON).click()
+        self.is_element_present(self.SPINNER_OFF)
+
+    @property
+    def press_save_new_document_button(self):
+        self.driver.find_element(*self.SAVE_DOCUMENT_BUTTON).click()
+        self.is_element_present(self.SPINNER_OFF)
+
+    def document_type_select(self, document_type):
+        """
+        Method select type of document in select menu
+        :param document_type: String document type. If type exists in select menu, then method will click on it, else will leave default value
+        :return:
+        """
+        Select(self.driver.find_element(*self.DOCUMENT_CATEGORY_SELECT)).select_by_visible_text(document_type)
+
+    def document_name_select(self, document_name):
+        """
+        Method select name of document in select menu
+        :param document_name: String document name. If name exists in select menu, then method will click on it, else will leave default value
+        :return:
+        """
+        Select(self.driver.find_element(*self.DOCUMENT_NAME_SELECT)).select_by_visible_text(document_name)
+
+    def set_document_series(self, chars):
+        """
+        Method sets the documents series
+        :param chars: String parametr.
+        :return:
+        """
+        self.driver.find_element(*self.DOCUMENT_SERIES_INPUT).send_keys(chars)
+
+    def set_document_number(self, number):
+        """
+        Method sets the documents number
+        :param number: Integer parametr.
+        :return:
+        """
+        self.driver.find_element(*self.DOCUMENT_NUMBER_INPUT).send_keys(number)
+
+    def set_day_of_issue(self, day_of_issue):
+        """
+        Method sets documents day of issue
+        :param day_of_issue: Date, when document was issued in a String format "YYYY-MM-DD"
+        :return:
+        """
+        day_of_issue_elem = self.driver.find_element(*self.DOCUMENT_DAY_OF_ISSUE_CHOSER)
+        day_of_issue_elem.clear()
+        day_of_issue_elem.send_keys(day_of_issue)
+
+    def set_document_maker(self, maker):
+        """
+        Method sets the organization which document was issued by
+        :param number: String parametr.
+        :return:
+        """
+        self.driver.find_element(*self.DOCUMENT_ISSUED_BY).send_keys(maker)
+
+    def check_is_original_document(self, is_original):
+        """
+        Method checks or unchecks "Is original document?" checkbox
+        :param is_original: Boolean format. Must be True, if document was compared with original.
+        :return:
+        """
+        self.checkbox_manager(self.driver.find_element(*self.DOCUMENT_IS_ORIGINAL), is_original)
+
+    def check_is_foreign_document(self, is_foreign):
+        """
+        Method checks or unchecks "Is foreign document?" checkbox
+        :param is_foreign: Boolean format. Must be True, if document is foreign.
+        :return:
+        """
+        self.checkbox_manager(self.driver.find_element(*self.DOCUMENT_IS_FOREIGN), is_foreign)
