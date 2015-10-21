99e3dd2cda29696b07a88d5c2efe07997b6adaf8
moshchonskiy <moshchonskiy@gmail.com>
2015-10-21 14:31:34 +0300

    attachments added

--- tests/test_search_filters_enrollment.py
+++ tests/test_search_filters_enrollment.py
@@ -49,8 +49,8 @@ class TestSearchFilters(object):
                 assert expected_in_search in actual_search_results
             except AssertionError:
                 allure.attach('screenshot', en_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
-                traceback.print_exc(file=sys.stdout)
-                pytest.xfail("failing configuration (but should work)")
+                # traceback.print_exc(file=sys.stdout)
+                pytest.fail(pytrace=True)
 
 
     # def test_filter_by_budget(self, app):
