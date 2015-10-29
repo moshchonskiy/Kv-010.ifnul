81002b62b282b4ec23e8b2d6cc4669b58bc662a3
moshchonskiy <moshchonskiy@gmail.com>
2015-10-29 18:44:24 +0200

    added allure steps added screenshots

--- tests/person_suite/4_filter_person_test.py
+++ tests/person_suite/4_filter_person_test.py
@@ -1,95 +1,155 @@
 # coding: utf8
 from model.user import User
-
+import pytest
+from allure.constants import AttachmentType
+import allure
+import sys
+import traceback
 
 def login(app):
     app.ensure_logout()
     app.login(User.Admin(), True)
 
-
+@pytest.allure.severity(pytest.allure.severity_level.NORMAL)
 def test_gender_filter(app):
-    login(app)
-    person_page = app.persons_page
-
-    person_page.try_get_gender_male_checkbox().click()
-    person_page.try_get_refresh_upper_button().click()
-    cells_texts = person_page.try_get_filtered_gender_column(u'Чоловіча')
-    for text in cells_texts:
-        assert text == u'Чоловіча'
+    with pytest.allure.step('Authorize to the application'):
+        login(app)
+        person_page = app.persons_page
+        person_page.try_get_gender_male_checkbox().click()
+        person_page.try_get_refresh_upper_button().click()
+    with pytest.allure.step('Assert the gender in column is male'):
+        try:
+            cells_texts = person_page.try_get_filtered_gender_column(u'Чоловіча')
+            for text in cells_texts:
+                assert text == u'Чоловіча'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
     person_page.try_get_close_filter().click()
-
     person_page.try_get_gender_female_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
-    cells_texts = person_page.try_get_filtered_gender_column(u'Жіноча')
-    for text in cells_texts:
-        assert text == u'Жіноча'
+    with pytest.allure.step('Assert the gender in column is female'):
+        try:
+            cells_texts = person_page.try_get_filtered_gender_column(u'Жіноча')
+            for text in cells_texts:
+                assert text == u'Жіноча'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
 
+@pytest.allure.severity(pytest.allure.severity_level.NORMAL)
 def test_type_filter(app):
     login(app)
     person_page = app.persons_page
-
     person_page.try_get_type_applicant_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
-    cells_texts = person_page.try_get_filtered_type_column(u'абітурієнт')
-    for text in cells_texts:
-        assert text == u'абітурієнт'
+    with pytest.allure.step('Assert the filtered information in column is applicant'):
+        try:
+            cells_texts = person_page.try_get_filtered_type_column(u'абітурієнт')
+            for text in cells_texts:
+                assert text == u'абітурієнт'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
     person_page.try_get_close_filter().click()
-
     person_page.try_get_type_student_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
-    cells_texts = person_page.try_get_filtered_type_column(u'студент')
-    for text in cells_texts:
-        assert text == u'студент'
+    with pytest.allure.step('Assert the filtered information in column is student'):
+        try:
+            cells_texts = person_page.try_get_filtered_type_column(u'студент')
+            for text in cells_texts:
+                assert text == u'студент'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
     person_page.try_get_close_filter().click()
-
     person_page.try_get_type_scientist_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
-    cells_texts = person_page.try_get_filtered_type_column(u'науковець')
-    for text in cells_texts:
-        assert text == u'науковець'
+    with pytest.allure.step('Assert the filtered information in column is scientist'):
+        try:
+            cells_texts = person_page.try_get_filtered_type_column(u'науковець')
+            for text in cells_texts:
+                assert text == u'науковець'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
     person_page.try_get_close_filter().click()
-
     person_page.try_get_type_employee_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
-    cells_texts = person_page.try_get_filtered_type_column(u'працівник')
-    for text in cells_texts:
-        assert text == u'працівник'
+    with pytest.allure.step('Assert the filtered information in column is employee'):
+        try:
+            cells_texts = person_page.try_get_filtered_type_column(u'працівник')
+            for text in cells_texts:
+                assert text == u'працівник'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
     person_page.try_get_close_filter().click()
-
     person_page.try_get_type_graduate_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
-    cells_texts = person_page.try_get_filtered_type_column(u'випускник')
-    for text in cells_texts:
-        assert text == u'випускник'
+    with pytest.allure.step('Assert the filtered information in column is graduate'):
+        try:
+            cells_texts = person_page.try_get_filtered_type_column(u'випускник')
+            for text in cells_texts:
+                assert text == u'випускник'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
     person_page.try_get_close_filter().click()
-
     person_page.try_get_type_outsider_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
-    cells_texts = person_page.try_get_filtered_type_column(u'сторонній')
-    for text in cells_texts:
-        assert text == u'сторонній'
+    with pytest.allure.step('Assert the filtered information in column is outsider'):
+        try:
+            cells_texts = person_page.try_get_filtered_type_column(u'сторонній')
+            for text in cells_texts:
+                assert text == u'сторонній'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
 
-# only for need hostel
+@pytest.allure.severity(pytest.allure.severity_level.NORMAL)
 def test_need_hostel_filter(app):
     login(app)
     person_page = app.persons_page
-
     person_page.try_get_need_hostel_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
-    cells_texts = person_page.try_get_filtered_need_hostel_column(u'потреб. гуртож.')
-    for text in cells_texts:
-        assert text == u'потреб. гуртож.'
+    with pytest.allure.step('Assert the filtered information in column is need accommodation'):
+        try:
+            cells_texts = person_page.try_get_filtered_need_hostel_column(u'потреб. гуртож.')
+            for text in cells_texts:
+                assert text == u'потреб. гуртож.'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
 
-# only for bound to military person
+@pytest.allure.severity(pytest.allure.severity_level.NORMAL)
 def test_bound_to_military_filter(app):
     login(app)
     person_page = app.persons_page
@@ -100,13 +160,19 @@ def test_bound_to_military_filter(app):
 
     person_page.try_get_bound_to_military_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
+    with pytest.allure.step('Assert the filtered information in column is military'):
+        try:
+            cells_texts = person_page.try_get_filtered_military_column(u'ВЗ')
+            for text in cells_texts:
+                assert text == u'ВЗ'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
-    cells_texts = person_page.try_get_filtered_military_column(u'ВЗ')
-    for text in cells_texts:
-        assert text == u'ВЗ'
 
-
-# only for foreigner resident
+@pytest.allure.severity(pytest.allure.severity_level.NORMAL)
 def test_resident_filter(app):
     login(app)
     person_page = app.persons_page
@@ -115,7 +181,21 @@ def test_resident_filter(app):
     person_page.try_get_close_after_addition_button().click()
     person_page.try_get_resident_foreigner_checkbox().click()
     person_page.try_get_refresh_upper_button().click()
+    with pytest.allure.step('Assert the filtered information in column is foreigner'):
+        try:
+            cells_texts = person_page.try_get_filtered_resident_column(u'інозем.')
+            for text in cells_texts:
+                assert text == u'інозем.'
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        except AssertionError:
+            allure.attach('screenshot', person_page.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+            print_simple_stacktrace()
+            raise
 
-    cells_texts = person_page.try_get_filtered_resident_column(u'інозем.')
-    for text in cells_texts:
-        assert text == u'інозем.'
+
+def print_simple_stacktrace():
+        _, _, tb = sys.exc_info()
+        tb_info = traceback.extract_tb(tb)
+        filename, line, func, text = tb_info[-1]
+        print('An error occurred on line: {}, in statement: {}. The filename is: {}, and function is: {}.'
+              .format(line, text, filename, func))
