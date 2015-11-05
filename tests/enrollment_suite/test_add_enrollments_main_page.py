20b887f8d011a127228aab0836d86c5e9b62677d
Vadym Shevchenko <spawnkiev@gmail.com>
2015-11-05 17:56:44 +0200

    some fix

--- tests/enrollment_suite/test_add_enrollments_main_page.py
+++ tests/enrollment_suite/test_add_enrollments_main_page.py
@@ -7,25 +7,26 @@ from utils.fill_enrollment import FillEnrollment
 __author__ = 'stako'
 
 
-# def test_add_enrollments(app):
+# def test_add_enrollments(app):logout_login
 def add_enrollments(logout_login):
     """
     Method creates and adds the enrollment.
-    :param logout_login:
+    :param app:
     """
     with pytest.allure.step('Test of add enrollment.'):
+        app = logout_login
         name_of_json_file = "fill_enrollment_main.json"
         name_of_dictionary = "fill_data_enrollment"
-        enrollment = logout_login.enrollments_main_page.get_enrollment(name_of_json_file, name_of_dictionary)
-        logout_login.internal_page.enrollments_page_link.click()
-        assert logout_login.enrollments_page.is_this_page
-        logout_login.enrollments_page.is_this_page.click()
-        logout_login.enrollments_main_page.fill_enrollment(name_of_json_file, name_of_dictionary)
+        enrollment = app.enrollments_main_page.get_enrollment(name_of_json_file, name_of_dictionary)
+        app.internal_page.enrollments_page_link.click()
+        assert app.enrollments_page.is_this_page
+        app.enrollments_page.is_this_page.click()
+        app.enrollments_main_page.fill_enrollment(name_of_json_file, name_of_dictionary)
         fill_enrollment = FillEnrollment()
-        logout_login.enrollments_page.search_enrollment(logout_login.enrollments_page.SEARCH_METHOD["document_series"],
+        app.enrollments_page.search_enrollment(app.enrollments_page.SEARCH_METHOD["document_series"],
                                                enrollment.series_of_statements)
         enrollment_like_table = fill_enrollment.table_enrollment_from_json(name_of_json_file,
                                                                            name_of_dictionary)
-        logout_login.internal_page.is_element_present(logout_login.internal_page.SPINNER_OFF)
-        enrollment_in_table = logout_login.enrollments_page.search_enrollment_in_table()
+        app.internal_page.is_element_present(app.internal_page.SPINNER_OFF)
+        enrollment_in_table = app.enrollments_page.search_enrollment_in_table()
         assert enrollment_like_table == enrollment_in_table
