f4c71c72f5cfec527d99c9b70b9632837b712738
moshchonskiy <moshchonskiy@gmail.com>
2015-11-08 15:12:21 +0200

    some

--- tests/saucelabs/conftest.py
+++ tests/saucelabs/conftest.py
@@ -37,13 +37,13 @@ def generator_app_for_sauce(request, base_url):
     test_result = sauceclient.SauceClient(SAUCE_USER_NAME, SAUCE_API_KEY)
     driver = request.param
     yield Application(driver, base_url)
+    print('SauceOnDemandSessionID={} job-name={}'.format(str(driver.session_id), "my_job"))
     try:
         if sys.exc_info() == (None, None, None):
             test_result.jobs.update_job(driver.session_id, passed=True)
         else:
             test_result.jobs.update_job(driver.session_id, passed=False)
     finally:
-        # print('SauceOnDemandSessionID={} job-name={}'.format(str(driver.session_id), "my_job"))
         driver.quit()
 
 
--- tests/saucelabs/test_authorization.py
+++ tests/saucelabs/test_authorization.py
@@ -3,6 +3,6 @@ import pytest
 
 def test_valid_login(generator_app_for_sauce):
     with pytest.allure.step('Valid login test'):
-        print('SauceOnDemandSessionID={} job-name={}'.format(str(generator_app_for_sauce.login_page.driver.session_id),
-                                                             test_valid_login.__name__))
+        # print('SauceOnDemandSessionID={} job-name={}'.format(str(generator_app_for_sauce.login_page.driver.session_id),
+        #                                                      test_valid_login.__name__))
         assert generator_app_for_sauce.login_page.is_this_page
