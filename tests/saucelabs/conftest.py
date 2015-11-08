1f16dc9361d41cc648863b13dbc149c70ef79694
moshchonskiy <moshchonskiy@gmail.com>
2015-11-08 12:06:33 +0200

    some

--- tests/saucelabs/conftest.py
+++ tests/saucelabs/conftest.py
@@ -41,14 +41,16 @@ def generator_app_for_sauce(request, base_url):
     test_result = sauceclient.SauceClient(SAUCE_USER_NAME, SAUCE_API_KEY)
     driver = request.param
     yield Application(driver, base_url)
-    print("Link to your job: https://saucelabs.com/jobs/%s" % driver.session_id)
     try:
         if sys.exc_info() == (None, None, None):
             test_result.jobs.update_job(driver.session_id, passed=True)
         else:
             test_result.jobs.update_job(driver.session_id, passed=False)
     finally:
+        print('SauceOnDemandSessionID={} job-name={}'.format(str(driver.session_id), "my_job"))
         driver.quit()
+
+
 
 
 
