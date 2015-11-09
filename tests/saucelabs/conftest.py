af0e3d2b41d3d613ebbabd0f9eb2510c946673f7
moshchonskiy <moshchonskiy@gmail.com>
2015-11-09 23:06:12 +0200

    new test

--- tests/saucelabs/conftest.py
+++ tests/saucelabs/conftest.py
@@ -10,36 +10,35 @@ from model.application import Application
 import os
 import sys
 import sauceclient
-import traceback
 
-SAUCE_USER_NAME = os.environ['SAUCE_USER_NAME']
-SAUCE_API_KEY = os.environ['SAUCE_API_KEY']
+sauce_user_name = os.environ['SAUCE_USER_NAME']
+sauce_api_key = os.environ['SAUCE_API_KEY']
 
-
-def get_remote_saucelabs_webdriver():
+def get_remote_browsers():
     SAUCE_ONDEMAND_BROWSERS = os.environ['SAUCE_ONDEMAND_BROWSERS']
     different_settings = json.loads(SAUCE_ONDEMAND_BROWSERS)
-    SAUCE_URL = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
-    drivers = []
+    browsers = []
     for setting in different_settings:
-        SELENIUM_PLATFORM = setting["platform"]
-        SELENIUM_VERSION = setting["browser-version"]
-        SELENIUM_BROWSER = setting["browser"]
-        desired_cap = {'browserName': SELENIUM_BROWSER, 'platform': SELENIUM_PLATFORM, 'version': SELENIUM_VERSION}
-        driver = webdriver.Remote(
-            command_executor=SAUCE_URL % (SAUCE_USER_NAME, SAUCE_API_KEY),
-            desired_capabilities=desired_cap)
-        drivers.append(driver)
-    return drivers
+        browsers.append({"platform":setting["platform"],
+                       "browserName":setting["browser"],
+                       "version":setting["browser-version"]
+                       })
+    return browsers
 
-
-@pytest.yield_fixture(scope="module", params=get_remote_saucelabs_webdriver())
+@pytest.yield_fixture(scope="module", params=get_remote_browsers())
 def generator_app_for_sauce(request, base_url):
-    driver = request.param
+    sauce_url = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
+    selenium_platform = request.param["platform"]
+    selenium_version = request.param["browser-version"]
+    selenium_browser = request.param["browser"]
+    desired_cap = {'browserName': selenium_browser, 'platform': selenium_platform, 'version': selenium_version}
+    driver = webdriver.Remote(
+            command_executor=sauce_url % (sauce_user_name, sauce_api_key),
+            desired_capabilities=desired_cap)
     print('SauceOnDemandSessionID={} job-name={}'.format(str(driver.session_id), request.param.name))
     yield Application(driver, base_url)
     driver.quit()
-    test_result = sauceclient.SauceClient(SAUCE_USER_NAME, SAUCE_API_KEY)
+    test_result = sauceclient.SauceClient(sauce_user_name, sauce_api_key)
     status = (sys.exc_info() == (None, None, None))
     test_result.jobs.update_job(driver.session_id, passed=status)
 
