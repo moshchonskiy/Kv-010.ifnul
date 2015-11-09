d4e6796b27b0e4efe434bdb7734bf2dac09f1b65
moshchonskiy <moshchonskiy@gmail.com>
2015-11-09 20:40:30 +0200

    new test

--- tests/saucelabs/test_authorization.py
+++ tests/saucelabs/test_authorization.py
@@ -1,9 +1,11 @@
 __author__ = 'Evgen'
 import pytest
 
-def test_valid_login(generator_app_for_sauce):
-    with pytest.allure.step('Valid login test'):
-        assert generator_app_for_sauce.login_page.is_this_page
+class TestGoogle:
 
-def test_invalid(generator_app_for_sauce):
-    assert generator_app_for_sauce.login_page.is_this_page
+    def test_valid_login(self, generator_app_for_sauce):
+        with pytest.allure.step('Valid login test'):
+            assert generator_app_for_sauce.login_page.is_this_page
+
+    def test_invalid(self, generator_app_for_sauce):
+        assert generator_app_for_sauce.login_page.is_this_page
